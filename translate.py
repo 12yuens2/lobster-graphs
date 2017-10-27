6dabbfd1a88fb380787d7ed11a823b1d15283cc2
12yuens2 <sizhe1007@gmail.com>
2017-10-27 11:51:04 +0100

    Modify translate to take multiple files.

--- translate.py
+++ translate.py
@@ -1,31 +1,42 @@
-input = open("graphs/lobster1388.gdf")
+import sys
+
+print("args: " + str(sys.argv))
+
+
+
 output = open("output.lne", "w")
 
-lines = input.read().splitlines()[1:]
+i = 0
+for input in sys.argv[1:]:
+    input_file = open("graphs/" + str(input))
 
-nodes = []
-edges = []
+    lines = input_file.read().splitlines()[1:]
 
-is_node = True
-for line in lines:
-    a = line.split(",")
-    if "edge" in line:
-        is_node = False
-    elif is_node:
-        nodes.append(int(a[0]))
-    else:
-        edges.append((int(a[0]), int(a[1])))
+    nodes = []
+    edges = []
 
-output.write("#graph0\n")
-output.write(str(len(nodes)) + "\n")
-for n in nodes:
-    output.write(str(n) + "\n")
+    is_node = True
+    for line in lines:
+        a = line.split(",")
+        if "edge" in line:
+            is_node = False
+        elif is_node:
+            nodes.append(int(a[0]))
+        else:
+            edges.append((int(a[0]), int(a[1])))
+
+    output.write("#graph" + str(i) + "\n")
+    output.write(str(len(nodes)) + "\n")
+    for n in nodes:
+        output.write(str(n) + "\n")
 
 
-output.write(str(len(edges)) + "\n")
-for e in edges:
-    output.write(str(e[0]) + " " + str(e[1]) + "\n")
-   
+    output.write(str(len(edges)) + "\n")
+    for e in edges:
+        output.write(str(e[0]) + " " + str(e[1]) + "\n")
+    output.write("\n")
+    i += 1
+    
 print(nodes)
 print(edges)
 
