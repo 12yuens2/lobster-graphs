230af98800f3f8171b362c3dceb6b51a080d4839
Sizhe Yuen <sizhe1007@gmail.com>
2017-11-24 12:59:40 +0000

    Change translation to use Node and Edge classes.

--- translate.py
+++ translate.py
@@ -3,8 +3,8 @@ import os
 from probability import *
 
 output = open("output.gfu", "w")
-nodes = []
-edges = []
+
+all_edges = []
 i = 0
 j = 0
 for f in os.listdir(sys.argv[1]):
@@ -30,13 +30,24 @@ for f in os.listdir(sys.argv[1]):
                 first = False
                 first_value = int(a[0])
 
-            v = a[1]
-            if (v == ""):
+            n = Node(a[0], a[1], 0)
+            if (a[1] == ""):
                 print("Node " + a[0] + " missing label.")
                 sys.exit()
-            nodes.append(v)
+            nodes.append(n)
         else:
-            edges.append((int(a[0]) - first_value, int(a[1]) - first_value))
+            n1 = 0
+            n2 = 0
+            for node in nodes:
+                if node.node_id == a[0]:
+                    n1 = node
+            for node in nodes:
+                if node.node_id == a[1]:
+                    n2 = node
+            e = Edge(n1, n2, a[3])
+            edges.append(e)
+            all_edges.append(e)
+            #edges.append((int(a[0]) - first_value, int(a[1]) - first_value))
 
 
     j += len(nodes)
@@ -44,11 +55,15 @@ for f in os.listdir(sys.argv[1]):
     output.write("#graph" + str(i) + "\n")
     output.write(str(len(nodes)) + "\n")
     for n in nodes:
-        output.write(str(n) + "\n")
+        output.write(str(n.label) + "\n")
 
 
     output.write(str(len(edges)) + "\n")
     for e in edges:
-        output.write(str(e[0]) + " " + str(e[1]) + "\n")
+        output.write(str(e.n1.node_id) + " " + str(e.n2.node_id) + "\n")
     i += 1
+    
+# create db for edge distributions
+edge_db = {}
+for edge in all_edges:
     
