9b67faddca2a1048352d64e0061ccff68c6075d0
12yuens2 <sizhe1007@gmail.com>
2018-01-29 15:57:54 +0000

    Move graphs and init script.

--- work/init.sh
+++ work/init.sh
@@ -1,4 +1,12 @@
+
+# Translate graphs to query graphs
 python3 translate.py graphs/
+
+# Move queries
 mv *.querygfu queries
-./ggsxe -b -gfu output.gfu
-./ggsxe -f -gfu output.gfu --dir queries/
+
+# Build graph database
+./ggsxe -b -gfu db.gfu
+
+# Query graph database
+./ggsxe -f -gfu db.gfu --dir queries/
